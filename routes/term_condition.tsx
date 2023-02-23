74f0f63e3f751ab52c4a30d5805a1020af51cd8b
singhaltd <85279370+singhaltd@users.noreply.github.com>
2023-02-23 11:15:31 +0700

    first Comit ;

---
+++ routes/term_condition.tsx
@@ -0,0 +1,136 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function TermAndCondition() {
+  return (
+    <>
+      <Head>
+        <title>ຂໍ້ກຳນົດ ແລະ ເງື່ອນໄຂ</title>
+      </Head>
+      <div className="w-8/12 mx-auto py-3">
+        <div className="border px-3 py-5 rounded-xl">
+          <div class="flex">
+            {/* <div className="w-56">
+              <h2>ຂໍ້ກຳນົດ ແລະ ເງື່ອນໄຂ</h2>
+              <ul>
+                <li className="py-2 hover:bg-red-300 px-5 border rounded-xl mt-1">
+                  0001
+                </li>
+                <li className="py-2 hover:bg-red-300 px-5 border rounded-xl mt-1">
+                  0002
+                </li>
+                <li className="py-2 hover:bg-red-300 px-5 border rounded-xl mt-1">
+                  0002
+                </li>
+                <li className="py-2 hover:bg-red-300 px-5 border rounded-xl mt-1">
+                  0002
+                </li>
+              </ul>
+            </div> */}
+            <div className="flex-1 px-5">
+              <p>
+                At OZZAO, we respect and safeguard the privacy of all visitors
+                and users of our website and app. We are committed to
+                maintaining the secure collection, use and disclosure of any
+                personal information that you all provide to us. Hereinafter, we
+                provide the Privacy Policy of our website and app OZZAO in
+                English, Chinese and Lao languages. &#3735;&#3765;&#3784; OZZAO,
+                &#3742;&#3751;&#3713;&#3776;&#3758;&#3771;&#3762;&#3737;&#3763;&#3779;&#3722;&#3785;&#3749;&#3760;&#3732;&#3761;&#3738;&#3716;&#3751;&#3762;&#3745;&#3754;&#3771;&#3745;&#3742;&#3761;&#3737;&#3777;&#3749;&#3760;&#3713;&#3762;&#3737;&#3739;&#3771;&#3713;&#3739;&#3785;&#3757;&#3591;&#3716;&#3751;&#3762;&#3745;&#3749;&#3761;&#3738;&#3714;&#3757;&#3719;&#3740;&#3769;&#3785;&#3776;&#3714;&#3771;&#3785;&#3762;&#3776;&#3746;&#3767;&#3784;&#3757;&#3719;&#3777;&#3749;&#3760;&#3740;&#3769;&#3785;&#3779;&#3722;&#3785;&#3714;&#3757;&#3719;&#3776;&#3751;&#3761;&#3738;&#3780;&#3722;&#3735;&#3788;&#3777;&#3749;&#3760;&#3777;&#3757;&#3739;&#3742;&#3749;&#3764;&#3776;&#3716;&#3722;&#3761;&#3737;&#3714;&#3757;&#3719;&#3742;&#3751;&#3713;&#3776;&#3758;&#3771;&#3762;.
+                &#3742;&#3751;&#3713;&#3776;&#3758;&#3771;&#3762;&#3720;&#3760;&#3758;&#3761;&#3738;&#3745;&#3767;&#3785;&#3737;&#3735;&#3765;&#3784;&#3720;&#3760;&#3738;&#3789;&#3784;&#3776;&#3757;&#3765;&#3785;&#3737;&#3739;&#3760;&#3720;&#3763;&#3713;&#3762;&#3737;&#3737;&#3763;&#3779;&#3722;&#3785;,
+                &#3713;&#3762;&#3737;&#3779;&#3722;&#3785;&#3777;&#3749;&#3760;&#3713;&#3762;&#3737;&#3779;&#3755;&#3785;&#3776;&#3755;&#3732;&#3740;&#3771;&#3737;&#3714;&#3757;&#3719;&#3714;&#3789;&#3785;&#3745;&#3769;&#3737;&#3754;&#3784;&#3751;&#3737;&#3733;&#3771;&#3751;&#3735;&#3765;&#3784;&#3735;&#3784;&#3762;&#3737;&#3776;&#3754;&#3765;&#3745;&#3714;&#3757;&#3719;&#3742;&#3751;&#3713;&#3776;&#3758;&#3771;&#3762;.
+                &#3733;&#3789;&#3784;&#3745;&#3762;,
+                &#3742;&#3751;&#3713;&#3776;&#3758;&#3771;&#3762;&#3720;&#3760;&#3779;&#3755;&#3785;&#3714;&#3789;&#3785;&#3745;&#3769;&#3737;&#3737;&#3763;&#3776;&#3734;&#3764;&#3719;&#3776;&#3719;&#3767;&#3784;&#3757;&#3737;&#3780;&#3714;&#3716;&#3751;&#3762;&#3745;&#3749;&#3761;&#3738;&#3749;&#3760;&#3738;&#3771;&#3738;&#3714;&#3757;&#3719;&#3776;&#3751;&#3738;&#3780;&#3722;&#3735;&#3788;&#3777;&#3749;&#3760;&#3777;&#3757;&#3739;&#3742;&#3749;&#3764;&#3776;&#3716;&#3722;&#3761;&#3737;&#3714;&#3757;&#3719;&#3742;&#3751;&#3713;&#3776;&#3758;&#3771;&#3762;.
+              </p>
+
+              <p>Privacy Policy</p>
+
+              <p>
+                At OZZAO, we use the highest level of security and privacy to
+                protect our customers and users. We are committed to protecting
+                the privacy and confidentiality of personal data and understand
+                the security implications of collecting and handling data.
+              </p>
+
+              <p>
+                This Privacy Policy (&ldquo;Policy&rdquo;) applies to our
+                website and mobile application (&quot;Service&quot;) and
+                explains how we collect and use personal data, how long we store
+                it and how users can access and manage their data.
+              </p>
+
+              <p>Information We Collect:</p>
+
+              <p>
+                We collect information that is necessary for us to deliver the
+                Service and for you to use it. This includes your name, contact
+                information and any other data that you voluntarily provide. We
+                may also collect and use other data to better understand how you
+                use and interact with our Service.
+              </p>
+
+              <p>Use of the collected Information:</p>
+
+              <p>We may use the information collected in the following ways:</p>
+
+              <p>
+                1. To provide and improve our Service 2. To improve customer
+                service and support 3. To communicate with you 4. To measure and
+                improve the effectiveness of our Service 5. To detect, prevent
+                and address technical issues
+              </p>
+
+              <p>Information Sharing and Disclosure:</p>
+
+              <p>
+                We respect the privacy of our customers and users and we will
+                not share any information about our customers or users with
+                third parties, except in instances where we have obtained your
+                consent or where it is required for legal, safety or security
+                reasons.
+              </p>
+
+              <p>Data Security:</p>
+
+              <p>
+                We have implemented physical and technical safeguards to protect
+                all data, whether we process it or store it on our Service. All
+                data is encrypted and kept secure, with access limited to
+                authorized personnel only.
+              </p>
+
+              <p>Retention of Data:</p>
+
+              <p>
+                We will retain data for as long as necessary to provide the
+                Service. If you wish to delete your data, you may do so by
+                contacting us at the address provided in this Policy.
+              </p>
+
+              <p>Your Consent:</p>
+
+              <p>
+                By using our Service, you consent to our collection and use of
+                your data in accordance with this Policy. If you do not agree to
+                this Policy, please do not use our Service.
+              </p>
+
+              <p>Changes to this Policy:</p>
+
+              <p>
+                Our Privacy Policy may change from time to time. Any changes to
+                this Policy will be reflected in this document and we will
+                notify you of any significant or material changes.
+              </p>
+
+              <p>Contact Us:</p>
+
+              <p>
+                If you have any questions about this Policy or about our data
+                processing activities, please contact us at info@ozzao.com.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
